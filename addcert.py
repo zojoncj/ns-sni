757dda9913a235bab67080a3d3baea5f34e3fa7a
Josh Zojonc <zojoncj@oregonstate.edu>
2018-03-28 14:36:05 -0700

    Ensureing files exist before we try to upload

--- addcert.py
+++ addcert.py
@@ -4,41 +4,28 @@ import argparse
 import sys,os
 import ConfigParser
 
+# Used to connect to the device
 from nssrc.com.citrix.netscaler.nitro.service.nitro_service import nitro_service
 from nssrc.com.citrix.netscaler.nitro.exception.nitro_exception import nitro_exception
 
-global config
+# Used to upload the cert & key
+from nssrc.com.citrix.netscaler.nitro.resource.config.system.systemfile import systemfile
 
-def main(arguments):
+def checkfile(filename):
+  if(not os.path.isfile(filename)):
+    sys.exit("File %s does not exist" % filename)
+  return filename
 
+def uploadfile(filename):
+  print("Uploading file: %s" %filename)
 
+def logout():
+  print("Logging out")
+  session.logout()
 
-  parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument("-u", "--url", help="URL of site being added ie. sig.o.e" , required=True)
-  parser.add_argument("-k", "--keyfile", help="Full path to key file", required=True  )
-  parser.add_argument("-c", "--certfile", help="Full Path to certificate file", required=True )
-  parser.add_argument("--config", help="Config file",default="./config" )
-
-  args = parser.parse_args(arguments)
-  configfile= args.config
-  keyfile = args.keyfile
-  cert = args.certfile
-  url = args.url
-  if(not os.path.isfile(configfile)):
-    sys.exit("Config file %s does not exist" % configfile)
-
-  
-  config = ConfigParser.ConfigParser()
-  config.read(configfile)
-
-  nsip = config.get('default','ns_ip')
-  username = config.get('default','ns_username')
-  password = config.get('default','ns_password')
-
-  ns_session = nitro_service(nsip,"HTTPS")
-  ns_session.set_credential(username,password)
+def connect(ip,user,passwd):
+  ns_session = nitro_service(ip,"HTTPS")
+  ns_session.set_credential(user,passwd)
 
   ns_session.timeout=30
 
@@ -46,6 +33,38 @@ def main(arguments):
     ns_session.login()
   except nitro_exception as  e:
     print("Exception::errorcode="+str(e.errorcode)+",message="+ e.message)
+
+  print("Connected to: %s as %s" %(ip,user))
+  return ns_session
+
+def main(arguments):
+  parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser.add_argument("-u", "--url", help="URL of site being added ie. sig.o.e" , required=True)
+  parser.add_argument("-k", "--keyfile", help="Full path to key file", required=True  )
+  parser.add_argument("-c", "--certfile", help="Full Path to certificate file", required=True )
+  parser.add_argument("--config", help="Config file",default="./config" )
+  args = parser.parse_args(arguments)
+
+  configfile= checkfile(args.config)
+  keyfile = checkfile(args.keyfile)
+  cert = checkfile(args.certfile)
+  url = args.url
+
+  config = ConfigParser.ConfigParser()
+  config.read(configfile)
+
+  nsip = config.get('default','ns_ip')
+  username = config.get('default','ns_username')
+  password = config.get('default','ns_password')
+  global session
+  session = connect(nsip,username,password)
+
+
+
+
+  logout()
 
 
 
